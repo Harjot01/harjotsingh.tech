f6a9c6ad7c3a09765d8c0b0adbaaea49630d57d3
Harjot Singh <harjotsingh.5125@gmail.com>
2025-12-06 14:32:49 +0530

    Rolled back to previous version

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -33,14 +33,14 @@ const About = async () => {
             </h1>
           </Reveal>
           <div className="flex flex-col gap-y-3">
-            {/* <Reveal delay={0.3}>
+            <Reveal delay={0.3}>
               <p>
-                Systems Engineer @{" "}
+                Ex-Intern @{" "}
                 <span className="text-primary hover:bg-primary hover:text-white cursor-pointer">
-                  IBM
+                  Cognizant Technology Solutions
                 </span>{" "}
               </p>
-            </Reveal> */}
+            </Reveal>
             <Reveal delay={0.4}>
               <p>
                 Ex-Frontend Developer @{" "}
